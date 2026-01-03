7a59020c27c0a3f0afe8944bd78cbc17a6add196
Omar Ramadan <omar.ramadanpersonal@gmail.com>
2026-01-03 14:54:36 +0300

    optimized encoding service to use a hash instead of array lookup

--- app/services/base62_converter.rb
+++ app/services/base62_converter.rb
@@ -2,6 +2,8 @@ class Base62Converter
   ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".freeze
   BASE = ALPHABET.length
 
+  DECODE_MAP = ALPHABET.each_char.with_index.to_h.freeze
+
   def self.encode(number)
     return ALPHABET[0] if number == 0
 
@@ -17,10 +19,13 @@ class Base62Converter
   def self.decode(string)
     number = 0
     string.reverse.each_char.with_index do |char, index|
-      power = BASE**index
-      value = ALPHABET.index(char)
-      number += value * power
+      value = DECODE_MAP[char]
+
+      raise ArgumentError, "Invalid character #{char} in Base62 string" if value.nil?
+
+      number += value * (BASE**index)
     end
     number
   end
 end
+
